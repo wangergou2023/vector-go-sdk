7107d725a0bed97a2e39d6311bde2b28784d7d0d
frcfppud <filippo.forchino@gmail.com>
2022-11-30 16:46:25 +0100

    WIP

--- pkg/sdk-wrapper/sdk-wrapper-audio.go
+++ pkg/sdk-wrapper/sdk-wrapper-audio.go
@@ -37,54 +37,57 @@ func PlaySound(filename string, volume int) string {
 		println("File not found!")
 		return "failure"
 	}
-	var pcmFile []byte
-	if strings.Contains(filename, ".pcm") || strings.Contains(filename, ".raw") {
-		fmt.Println("Assuming already pcm")
-		pcmFile, _ = os.ReadFile(filename)
-	} else {
-		conOutput, conError := exec.Command("ffmpeg", "-y", "-i", filename, "-f", "s16le", "-acodec", "pcm_s16le", "-ar", "16000", "-ac", "1", "/tmp/output.pcm").Output()
-		if conError != nil {
-			fmt.Println(conError)
-			return conError.Error()
+
+	go func() {
+		var pcmFile []byte
+		if strings.Contains(filename, ".pcm") || strings.Contains(filename, ".raw") {
+			fmt.Println("Assuming already pcm")
+			pcmFile, _ = os.ReadFile(filename)
+		} else {
+			conOutput, conError := exec.Command("ffmpeg", "-y", "-i", filename, "-f", "s16le", "-acodec", "pcm_s16le", "-ar", "16000", "-ac", "1", "/tmp/output.pcm").Output()
+			if conError != nil {
+				fmt.Println(conError)
+			}
+			fmt.Println("FFMPEG output: " + string(conOutput))
+			pcmFile, _ = os.ReadFile("/tmp/output.pcm")
 		}
-		fmt.Println("FFMPEG output: " + string(conOutput))
-		pcmFile, _ = os.ReadFile("/tmp/output.pcm")
-	}
-	var audioChunks [][]byte
-	for len(pcmFile) >= 1024 {
-		audioChunks = append(audioChunks, pcmFile[:1024])
-		pcmFile = pcmFile[1024:]
-	}
-	var audioClient vectorpb.ExternalInterface_ExternalAudioStreamPlaybackClient
-	audioClient, _ = Robot.Conn.ExternalAudioStreamPlayback(
-		ctx,
-	)
-	audioClient.SendMsg(&vectorpb.ExternalAudioStreamRequest{
-		AudioRequestType: &vectorpb.ExternalAudioStreamRequest_AudioStreamPrepare{
-			AudioStreamPrepare: &vectorpb.ExternalAudioStreamPrepare{
-				AudioFrameRate: 16000,
-				AudioVolume:    uint32(volume),
-			},
-		},
-	})
-	fmt.Println(len(audioChunks))
-	for _, chunk := range audioChunks {
+		var audioChunks [][]byte
+		for len(pcmFile) >= 1024 {
+			audioChunks = append(audioChunks, pcmFile[:1024])
+			pcmFile = pcmFile[1024:]
+		}
+		var audioClient vectorpb.ExternalInterface_ExternalAudioStreamPlaybackClient
+		audioClient, _ = Robot.Conn.ExternalAudioStreamPlayback(
+			ctx,
+		)
 		audioClient.SendMsg(&vectorpb.ExternalAudioStreamRequest{
-			AudioRequestType: &vectorpb.ExternalAudioStreamRequest_AudioStreamChunk{
-				AudioStreamChunk: &vectorpb.ExternalAudioStreamChunk{
-					AudioChunkSizeBytes: 1024,
-					AudioChunkSamples:   chunk,
+			AudioRequestType: &vectorpb.ExternalAudioStreamRequest_AudioStreamPrepare{
+				AudioStreamPrepare: &vectorpb.ExternalAudioStreamPrepare{
+					AudioFrameRate: 16000,
+					AudioVolume:    uint32(volume),
 				},
 			},
 		})
-		time.Sleep(time.Millisecond * 30)
-	}
-	audioClient.SendMsg(&vectorpb.ExternalAudioStreamRequest{
-		AudioRequestType: &vectorpb.ExternalAudioStreamRequest_AudioStreamComplete{
-			AudioStreamComplete: &vectorpb.ExternalAudioStreamComplete{},
-		},
-	})
-	os.Remove("/tmp/output.pcm")
+		fmt.Println(len(audioChunks))
+		for _, chunk := range audioChunks {
+			audioClient.SendMsg(&vectorpb.ExternalAudioStreamRequest{
+				AudioRequestType: &vectorpb.ExternalAudioStreamRequest_AudioStreamChunk{
+					AudioStreamChunk: &vectorpb.ExternalAudioStreamChunk{
+						AudioChunkSizeBytes: 1024,
+						AudioChunkSamples:   chunk,
+					},
+				},
+			})
+			time.Sleep(time.Millisecond * 30)
+		}
+		audioClient.SendMsg(&vectorpb.ExternalAudioStreamRequest{
+			AudioRequestType: &vectorpb.ExternalAudioStreamRequest_AudioStreamComplete{
+				AudioStreamComplete: &vectorpb.ExternalAudioStreamComplete{},
+			},
+		})
+		os.Remove("/tmp/output.pcm")
+	}()
+
 	return "success"
 }
 
